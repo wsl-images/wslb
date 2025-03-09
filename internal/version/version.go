6951a6cfa9da2be438513a830bf5001180caf38f
Steven Buglione <stevenbuglione1@gmail.com>
2025-03-09 01:00:42 -0500

    Updating how version.go works

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,9 +1,28 @@
 package version
 
-var (
-	Version = "dev"
+import (
+	"fmt"
+	"runtime/debug"
+	"strings"
 )
 
+var Version = "dev"
+
 func GetVersionInfo() string {
-	return "WSLB version: " + Version
+	version := Version
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "(devel)" && info.Main.Version != "" {
+			version = strings.TrimPrefix(info.Main.Version, "v")
+		}
+
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				version = fmt.Sprintf("%s (commit: %s)", version, setting.Value[:7])
+				break
+			}
+		}
+	}
+
+	return "WSLB version: " + version
 }
