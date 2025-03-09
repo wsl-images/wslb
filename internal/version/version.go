e72ef9c107f145a4ffde8d083da26264651531e0
Steven Buglione <stevenbuglione1@gmail.com>
2025-03-09 01:14:32 -0500

    Hard Coding Version

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,28 +1,9 @@
 package version
 
-import (
-	"fmt"
-	"runtime/debug"
-	"strings"
+var (
+	Version = "1.0.0"
 )
 
-var Version = "dev"
-
 func GetVersionInfo() string {
-	version := Version
-
-	if info, ok := debug.ReadBuildInfo(); ok {
-		if info.Main.Version != "(devel)" && info.Main.Version != "" {
-			version = strings.TrimPrefix(info.Main.Version, "v")
-		}
-
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
-				version = fmt.Sprintf("%s (commit: %s)", version, setting.Value[:7])
-				break
-			}
-		}
-	}
-
-	return "WSLB version: " + version
+	return "WSLB version: " + Version
 }
